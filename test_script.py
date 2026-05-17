23ce6090176f05f4741337054f1d9b5a61d305e5
JoyAshraf <147660626+JoyAshraf@users.noreply.github.com>
2026-05-18 02:23:40 +0300

    Added Classification

---
+++ test_script.py
@@ -0,0 +1,33 @@
+
+Classification
+def Preprocessing():
+    read csv('c')
+    binning
+    handling nulls (new)
+    enconding Sym
+    encoding features
+    scaling
+    feature selection  -> list names final features df[]
+
+def read_pickles():
+
+
+def run_models():
+    y_pred = predict(x)
+    accuracy = y y-pred
+    print
+
+Regression
+def Preprocessing():
+    read csv('r')
+    binning
+    handling nulls (new)
+    enconding Sym  
+    encoding features different nominal -> doesn't handle unknown categories
+    scaling 
+    feature selection  -> list names final features df[] -> different features
+
+def run_models():
+    y_pred = predict(x)
+    r2, mse
+    print
